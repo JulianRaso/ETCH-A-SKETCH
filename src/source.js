402af8d3e42dd90cc1e4531a6197910e3b5595cb
JulianRaso <julianraso@live.com>
2021-07-10 19:22:54 -0300

    Almost done

--- src/source.js
+++ src/source.js
@@ -1,19 +1,49 @@
+const body = document.querySelector('#game');
 const content = document.querySelector('#container');
+const grid = document.getElementsByClassName('container');
+const input = document.getElementById('number');
+const set = document.getElementById('button');
+const hover = documet.ge
 
+function gridBuild(){
+    var max = input.value;
 
+    if(max != ""){
 
+        content.style.width = "fit-content";
+        content.style.height = "";
 
-for(var i = 1; i <= 16; i++){
+        content.innerHTML = "";
 
-    for(var j = 1; j<= 16; j++){
-        const div = document.createElement('div');
+        // Grid Build
 
-        //  div.textContent = j.toString();
-        div.style.border = 'solid 2px'
-        div.style.margin = '1px' 
-        div.style.height = '20px'
-        div.style.width = '20px'
+        for(var i = 1; i <= max; i++){
+            for(var j = 1; j<= max; j++){
+                const div = document.createElement('div');
 
-        content.appendChild(div);
+                div.style.border = 'solid 2px'
+                div.style.height = '20px'
+                div.style.width = '20px'
+
+                content.appendChild(div);
+            }
+        }
+
     }
+
 }
+
+
+//Control of Input value
+
+input.addEventListener("change", number => {
+    if(input.value > 100){
+        input.value = 100;
+    }
+    else if(input.value < 16){
+        input.value = 16;
+    }
+})
+
+//Hover
+
